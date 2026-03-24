4571070535539f6ce0b49123ddcf96b97011e27f
unknown <wagner_lisboa@live.com>
2026-03-24 14:52:22 -0300

    feat: add order creation route and login with token validation; create login schema

--- order_routes.py
+++ order_routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import create_session
+from schemas import OrderSchema
+from models import Order
 order_router = APIRouter(prefix="/orders", tags=["orders"]) #Create a route for ordering
 
 @order_router.get("/")
@@ -9,3 +12,13 @@ async def orders():
     :return:
     """
     return {"mensage": "You accesed the standart order route"}
+
+@order_router.post("/order")
+async def create_order(order_schema: OrderSchema, session = Depends(create_session)):
+    """
+    :return:
+    """
+    new_order = Order(user=order_schema.user)
+    session.add(new_order)
+    session.commit()
+    return {"mensage": f"Order created successfully. Order ID: {new_order.id}"}
