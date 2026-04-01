db87613dfaea5383d4b4a3630bb901ee865b4387
unknown <wagner_lisboa@live.com>
2026-04-01 15:14:37 -0300

    feat: separate access and refresh tokens with type validation; improve authentication flow

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import Session
 from app.core.security import bcrypt_context
 from app.core.exceptions import EmailUsedException
 from app.repositories.auth_repository import get_user_by_email
+from app.core.config import ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+from datetime import datetime, timedelta, timezone
+from jose import jwt
 
 
 def authenticator_user(email, password, session):
@@ -43,3 +46,35 @@ def create_account_service(user_schema: UserSchema, session: Session) -> User:
     session.commit()
     session.refresh(new_user)
     return new_user
+
+
+def create_access_token(user_id) -> str:
+    """
+    Function to create encoded tokens based on the user id and time of expiration. Token standard JWT.
+    :param user_id: user owner of token
+    :param token_time: time of expiration of token.
+    :return: encode token
+    """
+    expiration_date = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    dic_info = {"sub": str(user_id), 
+                "type": "access",
+                "exp": expiration_date}
+    
+    return jwt.encode(dic_info, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def create_refresh_token(user_id) -> str:
+        """
+        Function to create encoded tokens based on the user id and time of expiration. Token standard JWT.
+        :param user_id: user owner of token
+        :param token_time: time of expiration of token.
+        :return: encode token
+        """
+        expiration_date = datetime.now(timezone.utc) + timedelta(days=7)
+
+        dic_info = {"sub": str(user_id), 
+                    "type": "refresh",
+                    "exp": expiration_date}
+        
+        return jwt.encode(dic_info, SECRET_KEY, algorithm=ALGORITHM)
