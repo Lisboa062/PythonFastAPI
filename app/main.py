a4cb7a7317b8a4c8f32940aa2426c199558015ac
unknown <wagner_lisboa@live.com>
2026-04-01 16:02:17 -0300

    feat: implementação da exceção global para itens não encontrados.

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from app.core.exception_handlers import (
     email_used_exception_handler,
     order_not_found_handler,
-    not_authorized_handler)
+    not_authorized_handler,
+    item_not_found_handler)
 
 from app.core.exceptions import (
     EmailUsedException,
     OrderNotFoundException,
-    NotAuthorizedException)
+    NotAuthorizedException,
+    ItemNotFoundException)
 
 app = FastAPI()
 
@@ -18,6 +20,7 @@ from app.routers.order_routes import order_router
 app.add_exception_handler(EmailUsedException, email_used_exception_handler)
 app.add_exception_handler(OrderNotFoundException, order_not_found_handler)
 app.add_exception_handler(NotAuthorizedException, not_authorized_handler)
+app.add_exception_handler(ItemNotFoundException, item_not_found_handler)
 
 app.include_router(auth_router)
 app.include_router(order_router)
