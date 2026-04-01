e4bfeef19a00cdb730a7eda32f12ceaa54d4cde6
unknown <wagner_lisboa@live.com>
2026-04-01 15:45:27 -0300

    feat: implement global exception handling; centralize error responses and decouple HTTP layer

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from app.core.exception_handlers import (
+    email_used_exception_handler,
+    order_not_found_handler,
+    not_authorized_handler)
+
+from app.core.exceptions import (
+    EmailUsedException,
+    OrderNotFoundException,
+    NotAuthorizedException)
 
 app = FastAPI()
 
 
 from app.routers.auth_routes import auth_router
 from app.routers.order_routes import order_router
+
+app.add_exception_handler(EmailUsedException, email_used_exception_handler)
+app.add_exception_handler(OrderNotFoundException, order_not_found_handler)
+app.add_exception_handler(NotAuthorizedException, not_authorized_handler)
 
 app.include_router(auth_router)
 app.include_router(order_router)
