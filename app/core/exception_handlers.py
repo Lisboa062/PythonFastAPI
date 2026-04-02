e428207b4e9f1dfb16c1bed3167c6313c6451f2b
unknown <wagner_lisboa@live.com>
2026-04-02 08:41:24 -0300

    feat: adicionar exception de credenciais inválidas e padronizar erros de autenticação

--- app/core/exception_handlers.py
+++ app/core/exception_handlers.py
@@ -4,7 +4,8 @@ from fastapi.responses import JSONResponse
 from app.core.exceptions import (EmailUsedException, 
                                  OrderNotFoundException, 
                                  NotAuthorizedException, 
-                                 ItemNotFoundException)
+                                 ItemNotFoundException,
+                                 InvalidCredentialsException)
 
 
 async def email_used_exception_handler(request: Request, exc: EmailUsedException):
@@ -21,12 +22,19 @@ async def order_not_found_handler(request: Request, exc: OrderNotFoundException)
 
 async def not_authorized_handler(request: Request, exc: NotAuthorizedException):
     return JSONResponse(
-        status_code=401,
+        status_code=403,
         content={"detail": "You are not authorized."},)
 
 
 async def item_not_found_handler(request: Request, exc: ItemNotFoundException):
     return JSONResponse(
-        status_code=401,
+        status_code=404,
         content={"detail":"Item not found."}
+    )
+
+
+async def invalid_credentials_handler(request: Request, exc: InvalidCredentialsException):
+    return JSONResponse(
+        status_code=401,
+        content={"detail":"Invalid Email or Password."}
     )
